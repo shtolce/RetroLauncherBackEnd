25c01295507e0930d983d235667b40bc20bf8f8a
Dmitry Kim <kimdimka@yandex.ru>
2019-08-16 07:17:36 +1000

    123123123

---
+++ WebApiRL/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApiRL.Model;
+using WebApiRL.Services;
+
+namespace WebApiRL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IConfiguration Config;
+        private ObservableCollection<Game> _games;
+        public ValuesController(IConfiguration config)
+        {
+            _games = new ObservableCollection<Game>(Services.RepositoryBase.GetGamesAsync(config).Result.Distinct());
+
+            this.Config = config;
+
+
+        }
+        
+        // GET api/values
+        [HttpGet]
+        public ObservableCollection<Game> Get()
+        {
+            
+            return _games;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiRL/Model/Game.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiRL.Model
+{
+    public class Game 
+    {
+        int gameId;
+        public int GameId
+        {
+            get { return gameId; }
+            set { gameId = value; }
+        }
+
+        public string Name { get; set; }
+        public string NameSecond { get; set; }
+        public string NameOther { get; set; }        
+        public string Genre { get; set; }
+        public int? Year { get; set; }
+        public string Developer { get; set; }
+
+        string annotation;
+        public string Annotation
+        {
+            get { return annotation; }
+            set { if (value.Length > 1000) annotation = value.Substring(0, 1000); else annotation = value; }
+        }
+
+        public Platform Platform { get; set; }
+
+        private List<GameLink> gameLinks { get; set; }
+        public List<GameLink> GameLinks
+        {
+            get { return gameLinks; }
+            set
+            {
+                gameLinks = value;
+            }
+        }
+
+        private string imgUrl;
+        public string ImgUrl
+        {
+            get
+            {
+               
+                if (GameLinks != null && string.IsNullOrEmpty(imgUrl))
+                {
+                    TaskAwaiter<string> awaiter = Services.RepositoryImage.GET(GameLinks.Where(lnk => lnk.TypeUrl == TypeUrl.MainScreen).FirstOrDefault().Url).GetAwaiter();
+                    awaiter.OnCompleted(() =>
+                    {
+                        imgUrl = awaiter.GetResult();
+                    });
+                }
+                return imgUrl;
+            }
+        }
+
+
+    }
+}
